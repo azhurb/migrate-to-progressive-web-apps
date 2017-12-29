453e8f7ce0bf506f3ed7f73f4e47541fbc02b4e2
alex <zhurbitsky@gmail.com>
2017-12-29 14:54:54 +0200

    Primer on Push Notifications

--- work/site.js
+++ work/site.js
@@ -12,7 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const publicKey = '<public key from Push Companion>';
+
 /** Intentionally empty */
 navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
     console.log('Excellent, registered with scope: ', registration.scope);
+});
+
+navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    serviceWorkerRegistration.pushManager.getSubscription()
+        .then(function(subscription) {
+            // subscription will be null or a PushSubscription
+            if (subscription) {
+                console.info('Got existing', subscription);
+                window.subscription = subscription;
+                return;  // got one, yay
+            }
+
+            const applicationServerKey = urlB64ToUint8Array(publicKey);
+            serviceWorkerRegistration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey,
+                })
+                .then(function(subscription) {
+                    console.info('Newly subscribed to push!', subscription);
+                    window.subscription = subscription;
+                });
+        });
 });
